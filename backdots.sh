a97ca66bc516a6dfafeb1c727a3a9768286d2084
jo <jo@ubuntu14.04-virtualbox>
2014-08-19 17:36:02 +0900

    implement backuping dotfile function.

--- backdots.sh
+++ backdots.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-# using sudoers to set up environment for sudo invocation.
+# assuming $HOME as working directory 
+BACKUP_DIR_NAME=dotfiles
 
+if [ -d ./$BACKUP_DIR_NAME ] ; then
+	echo "Backup has been already done."
+	echo "This process skips."
+	return 1
+fi
+
+/bin/mkdir $BACKUP_DIR_NAME && {
+  for DOT_FILE in ".[^.]*" ; do
+    echo $DOT_FILE 
+  done
+}
