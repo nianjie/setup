44fbe140daa0dd5aef77d4918d4d5723c0871c20
jo <jo@ubuntu14.04-virtualbox>
2014-08-20 17:22:08 +0900

    append detailed messages including the number of failed files as well as the names.

--- backdots.sh
+++ backdots.sh
@@ -2,6 +2,8 @@
 # assuming $HOME as working directory 
 BACKUP_DIR_NAME=dotfiles
 DOT_FILE_COUNTER=0
+FAILED_COUNTER=0
+FAILED_FILE_LIST=""
 
 if [ -d ./$BACKUP_DIR_NAME ] ; then
 	echo "Backup has been already done."
@@ -13,9 +15,13 @@ fi
   for DOT_FILE in .[!.]* ; do
     if [ -f $DOT_FILE ] ; then
         DOT_FILE_COUNTER=$(($DOT_FILE_COUNTER+1))
-    	cp "$DOT_FILE" "./$BACKUP_DIR_NAME" >/dev/null 2>&1 \
-	   || echo "$DOT_FILE: backup failed."
+    	cp "$DOT_FILE" "./$BACKUP_DIR_NAME" >/dev/null 2>&1  || \
+		{ FAILED_COUNTER=$(($FAILED_COUNTER+1))         \
+        	FAILED_FILE_LIST="$FAILED_FILE_LIST, $DOT_FILE"	; }
     fi
   done
   echo "All dotfiles: $DOT_FILE_COUNTER."
+  if [ $FAILED_COUNTER -gt 0 ] ; then 
+    echo "Backup failed: $FAILED_COUNTER($FAILED_FILE_LIST)."
+  fi
 }
