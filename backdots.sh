9eca1add90dddbf42845406febc06530beb601ae
jo <jo@ubuntu14.04-virtualbox>
2014-08-20 16:55:24 +0900

    count dotfiles.

--- backdots.sh
+++ backdots.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # assuming $HOME as working directory 
 BACKUP_DIR_NAME=dotfiles
+DOT_FILE_COUNTER=0
 
 if [ -d ./$BACKUP_DIR_NAME ] ; then
 	echo "Backup has been already done."
@@ -10,9 +11,11 @@ fi
 
 /bin/mkdir $BACKUP_DIR_NAME && {
   for DOT_FILE in .[!.]* ; do
+    DOT_FILE_COUNTER=$(($DOT_FILE_COUNTER+1))
     if [ -f $DOT_FILE ] ; then
     	cp "$DOT_FILE" "./$BACKUP_DIR_NAME" >/dev/null 2>&1 \
 	   || echo "$DOT_FILE: backup failed."
     fi
   done
+  echo "All dotfiles: $DOT_FILE_COUNTER."
 }
