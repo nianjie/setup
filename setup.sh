57a793ac0e70f5046a5f7018572f4a148ec192a3
jo <jo@ubuntu14.04>
2014-09-02 18:17:09 +0900

    fix bug of no nodejs being effective.

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.31
-nvm use v0.10.31
+#nvm use v0.10.31
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
